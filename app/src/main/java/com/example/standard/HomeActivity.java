284f1683c82c1f62a7c86460eb9a52b04671c17c
abdul <abdul.tanveer@gmail.com>
2023-08-21 15:44:19 +0530

    lifecycle of an activity

--- app/src/main/java/com/example/standard/HomeActivity.java
+++ app/src/main/java/com/example/standard/HomeActivity.java
@@ -4,23 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public static String TAG = HomeActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-      String data =   getIntent()   //get intent which started this activity
+        Log.i(TAG,"oncreate");
+
+      /*String data =   getIntent()   //get intent which started this activity
                 .getExtras()
                 .getString("sc");
         TextView tvResult = findViewById(R.id.tvResult);
-        tvResult.setText(data);
+        tvResult.setText(data);*/
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG,"onStart-get the data");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG,"onResume --restore the app state");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG,"onPause-- save app state");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG,"onStop -hibernate");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
     }
 
     public void handleSelection(View view) {
