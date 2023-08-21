774442c661d2faf82928a0d51443eb258f5c229c
abdul <abdul.tanveer@gmail.com>
2023-08-21 15:09:08 +0530

    getting data from another activity

--- app/src/main/java/com/example/standard/HomeActivity.java
+++ app/src/main/java/com/example/standard/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.standard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -18,5 +21,17 @@ public class HomeActivity extends AppCompatActivity {
         TextView tvResult = findViewById(R.id.tvResult);
         tvResult.setText(data);
 
+    }
+
+    public void handleSelection(View view) {
+        EditText conEditText = findViewById(R.id.etContact);
+        String contact = conEditText.getText().toString();
+        //put this data into an intent
+        Intent retIntent = new Intent();
+        retIntent.putExtra("con",contact);
+        //set the result -- the main activity knows whether the data is good to be consumed or rejected
+        setResult(RESULT_OK,retIntent);  //RESULT_OK -- smell
+        //close this activity
+        finish();
     }
 }
--- app/src/main/java/com/example/standard/MainActivity.java
+++ app/src/main/java/com/example/standard/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.standard;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -12,18 +14,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText etName;
+    TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        etName = findViewById(R.id.etName);
+        setContentView(R.layout.activity_main); //inflation -- layout inflater
+       // inflateLayout();
+         tvMain = findViewById(R.id.tvmain);
+        etName = findViewById(R.id.etName); //getting handle on the view
+    }
+
+    private void inflateLayout() {
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+
+       /* textView.setText("hello world");
+
+        constraintLayout.addView(textView);*/
     }
 
     public void handleClick(View view) {
         //get the value from edittext
         String name = etName.getText().toString();
         //take the handle of the textview and
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.tvmain);
         //change the content of the textview
         textView.setText(name);
     }
@@ -33,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btnHome:*/
                 Intent homeIntent = new Intent(this,HomeActivity.class); //explicit intent
                 homeIntent.putExtra("sc","android app dev at sc");
-                startActivity(homeIntent);
+                startActivityForResult(homeIntent,123);
               /*  break;
             case R.id.btnDial:
 
@@ -41,6 +54,18 @@ public class MainActivity extends AppCompatActivity {
         }*/
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent retIntent) {
+        super.onActivityResult(requestCode, resultCode, retIntent);
+        if(resultCode == RESULT_OK && requestCode ==123){ //if the result was not cancelled [stale food]
+            String contact = retIntent.getExtras().getString("con");
+            tvMain.setText(contact);
+        }
+    }
+
+
+
 
     public void launchDialer(View view) {
         /*Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+91-9880979732")); //implicit intent
